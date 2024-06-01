9796d4e6e287232e628477f5e44ef6aadf00dc9b
Daniel Camuffo <dankemedic@hotmail.com>
2024-06-01 22:05:11 +0200

    Added ifa_pak40base_fix

---
+++ addons/ifa_pak40base_fix/config.cpp
@@ -0,0 +1,49 @@
+#include "script_component.hpp"
+
+class CfgPatches { 
+	class ADDON {
+		name = "=BTC= IFA - Pak40Base Fix";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {"cba_xeh", "WW2_Assets_c_Vehicles_StaticWeapons_c", "fow_cannons_c"};
+		url = "https://www.blacktemplars.it/";
+		author = "=BTC= Black Templars Clan";
+        authors[] = {"=BTC= Fyuran"};
+		requiredVersion = "0.1";
+	};
+}; 
+
+class CfgVehicles {
+ 	class All;
+	class AllVehicles: All
+	{
+		class ViewOptics;
+		class NewTurret
+		{
+			class ViewGunner;
+		};
+	};
+	class Land: AllVehicles{};
+	class LandVehicle: Land
+	{
+		class ViewPilot;
+		class CommanderOptics;
+	};
+    class StaticWeapon: LandVehicle
+	{
+		class NewTurret;
+		class Turrets
+		{
+			class MainTurret;
+			class CommanderOptics: CommanderOptics {};
+			class ViewOptics: ViewOptics {};
+		};
+	};
+	class StaticCannon: StaticWeapon
+	{
+		class Turrets: Turrets
+		{
+			class MainTurret;
+		};
+	};
+};
---
+++ addons/ifa_pak40base_fix/script_component.hpp
@@ -0,0 +1,14 @@
+#define MAINPREFIX z
+#define PREFIX btc
+#define COMPONENT ifa_pak40base_fix
+
+#include "\x\cba\addons\main\script_macros_common.hpp"
+
+#ifdef DISABLE_COMPILE_CACHE
+    #undef PREP
+    #define PREP(fncName) DFUNC(fncName) = compile preprocessFileLineNumbers QPATHTOF(functions\DOUBLES(fnc,fncName).sqf)
+#else
+    #undef PREP
+    #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
+#endif
+
